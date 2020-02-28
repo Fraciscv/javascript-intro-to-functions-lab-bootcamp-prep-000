9d2d0e65237a93b46c612db598b49408602c71f1
Francisco Andaur <francisco.andaur@icloud.com>
2020-02-28 18:52:49 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function shout(s) {
+  return s.toUpperCase()
+}
+function whisper(s) {
+  return s.toLowerCase()
+}
+function logShout(s) {
+  console.log('HELLO')
+}
+function logWhisper(s) {
+  console.log('hello')
+}
+function sayHiToGrandma(s){
+  if (s === s.toLowerCase())
+  {return "I can't hear you!";}
+  if (s === s.toUpperCase())
+  {return "YES INDEED!"}
+  else {return "I love you, too."}
+}
